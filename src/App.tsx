bcca0f9b237c1982bf681080ca47d573b5bef953
Artur Pushkov <arturpushkov@MacBook-Pro-Artur.local>
2021-12-07 03:04:59 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const MAX_LEVEL = 5;
+const MIN_LEVEL = 0;
+const STEP = 1;
+
+enum Action {
+    INCREASE = "increase",
+    DECREASE = "decrease"
+}
+
+interface AppProps {
+    durationMs: number,
+    minLevel: number,
+    maxLevel: number
+}
+export const App: React.FC<Partial<AppProps>> = ({durationMs = 2000, minLevel = MIN_LEVEL, maxLevel = MAX_LEVEL}) => {
+    const [level, setLevel] = useState<number>(minLevel);
+    const [action, setAction] = useState<null | Action>(null);
+
+    if (maxLevel === 0 || maxLevel <= minLevel) {
+        throw Error("Invalid props")
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (action === Action.INCREASE && level < maxLevel) {
+                setLevel((prevWaterLevel) => prevWaterLevel + STEP);
+            }
+            if (action === Action.DECREASE && level > minLevel) {
+                setLevel((prevWaterLevel) => prevWaterLevel - STEP);
+            }
+        }, durationMs);
+
+        if (action === Action.DECREASE && level === minLevel) {
+            setAction(null);
+        }
+
+        if (action === Action.INCREASE && level === maxLevel) {
+            setAction(null);
+        }
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [action, level]);
+
+    const increaseLevel = async () => {
+        if (action === Action.INCREASE) {
+            setAction(null);
+            return;
+        }
+        setAction(Action.INCREASE);
+    }
+
+    const decreaseLevel = () => {
+        if (action === Action.DECREASE) {
+            setAction(null);
+            return;
+        }
+        setAction(Action.DECREASE);
+    }
+
+    return (
+        <>
+            <h1>Level: {level}</h1>
+            <div className="wrapper">
+                <div style={{height: 20 * maxLevel}}  className="bathtub">
+                    {Array.from(Array(level)).map((item, index) => {
+                        return <div key={index} className="water"/>
+                    })}
+                </div>
+                <button
+                    disabled={level === maxLevel || action === Action.DECREASE}
+                    onClick={increaseLevel}
+                    className="button"
+                >
+                    {action && action !== Action.DECREASE && level !== maxLevel ? "Stop" : "Increase"}
+                </button>
+                <button
+                    disabled={level === minLevel || action === Action.INCREASE}
+                    onClick={decreaseLevel}
+                    className="button"
+                >
+                    {action && action !== Action.INCREASE && level !== minLevel ? "Stop" : "Decrease"}
+                </button>
+            </div>
+        </>
+    );
 }
 
 export default App;
